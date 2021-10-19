108035ace4bc8b2e16e74e4b13d0baa16a5b6a04
balanjaneyulu23 <balanjaneyulu23@gmail.com>
2021-10-19 11:24:42 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         I am balaleela. Nagajayadeesh! we are three!
         </p>
         <a
           className="App-link"
